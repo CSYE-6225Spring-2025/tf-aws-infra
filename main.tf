87a438840e7c9e071c1bb7ba6a5de500aa42e711
chintaabhinav <chinta.abhi@northeastern.edu>
2025-02-25 13:35:30 -0500

    committ

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ provider "aws" {
 }
 
 # Create VPC
-resource
- "aws_vpc" "main" {
+resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
